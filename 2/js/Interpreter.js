d4be373e4ae7d56c010d693931569adbf3439ba5
DeadbraiN <deadbrainman@gmail.com>
2012-11-29 20:15:56 +0200

    * Total files structure refactoring. Part 3

--- 2/js/Interpreter.js
+++ 2/js/Interpreter.js
@@ -19,15 +19,17 @@ var fs     = require('fs');
 //
 // Entry point of application
 //
-    var scriptFile = process.argv.length > 2 ? process.argv[2] : '';
+var scriptFile = process.argv.length > 2 ? process.argv[2] : '';
 
 if (!Lib.Helper.isString(scriptFile)) {
     console.log('Invalid script file: ' + scriptFile);
-    return 1;
+    // TODO: constant should be moved upper
+    process.exit(1);
 }
 if (!fs.existsSync(scriptFile)) {
     console.log('Script file doesn\'t exists: ' + scriptFile);
-    return 1;
+    // TODO: constant should be moved upper
+    process.exit(1);
 }
 
 //
