ab086347d4377aa919471e5f3d53bea9341076ea
tmptrash <tmptrash@mail.ru>
2012-12-14 19:56:08 +0200

    + Added new project settings for JSLint/Hint * Fixed speech for 2 level

---
+++ 4/js/Scenario.js
@@ -0,0 +1,10 @@
+/**
+ * This is level 4 scenario class. It makes decisions regarded 3d objects, user input,
+ * terminal commands and so on. It controls all 3d effects and the scene...
+ *
+ * @author DeadbraiN
+ * @email deadbrainman@gmail.com
+ */
+App.Satellite = speculoos.Class({
+    extend: App.WebGlContainer
+});
