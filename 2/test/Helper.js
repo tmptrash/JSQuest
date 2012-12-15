0a35c09d41d8af106d6ffd91439f121630e6ce5a
DeadbraiN <deadbrainman@gmail.com>
2012-12-16 00:30:24 +0200

    * refactoring continue

--- 2/test/Helper.js
+++ 2/test/Helper.js
@@ -15,55 +15,55 @@ var NUnitHelper = {
      * @param {Function} cb Callback function
      */
     callWithAllTypes: function (msg, cb) {
-        assert.throws(function() {
+        assert.throws(function () {
             cb('Hello world!');
         }, undefined, msg + ' - not empty string');
-        assert.throws(function() {
+        assert.throws(function () {
             cb(' ');
         }, undefined, msg + ' - string with one space');
-        assert.throws(function() {
+        assert.throws(function () {
             cb('');
         }, undefined, msg + ' - empty string');
-        assert.throws(function() {
+        assert.throws(function () {
             cb(123);
         }, undefined, msg + ' - number 123');
-        assert.throws(function() {
+        assert.throws(function () {
             cb(0);
         }, undefined, msg + ' - number 0');
-        assert.throws(function() {
+        assert.throws(function () {
             cb(-1);
         }, undefined, msg + ' - number -1');
-        assert.throws(function() {
+        assert.throws(function () {
             cb(function () {});
         }, undefined, msg + ' - empty function');
-        assert.throws(function() {
+        assert.throws(function () {
             cb([]);
         }, undefined, msg + ' - empty array');
-        assert.throws(function() {
+        assert.throws(function () {
             cb([1,2,3]);
         }, undefined, msg + ' - not empty array');
-        assert.throws(function() {
+        assert.throws(function () {
             cb(NaN);
         }, undefined, msg + ' - NaN');
-        assert.throws(function() {
+        assert.throws(function () {
             cb(Infinity);
         }, undefined, msg + ' - Infinity');
-        assert.throws(function() {
+        assert.throws(function () {
             cb(undefined);
         }, undefined, msg + ' - undefined');
-        assert.throws(function() {
+        assert.throws(function () {
             cb(null);
         }, undefined, msg + ' - null');
-        assert.throws(function() {
+        assert.throws(function () {
             cb(true);
         }, undefined, msg + ' - boolean true');
-        assert.throws(function() {
+        assert.throws(function () {
             cb(false);
         }, undefined, msg + ' - boolean false');
-        assert.throws(function() {
+        assert.throws(function () {
             cb(/[^]/);
         }, undefined, msg + ' - regexp');
-        assert.throws(function() {
+        assert.throws(function () {
             cb({});
         }, undefined, msg + ' - object');
     },
@@ -92,7 +92,7 @@ var NUnitHelper = {
         var s = new Alan();
 
         if (!fs.existsSync(file)) {
-            throw new Error('Script file "' + file + '" does not exist.')
+            throw new Error('Script file "' + file + '" does not exist.');
         }
         if (hasError) {
             try {
