3614152135e38d1989eaeaaccbf8d18a1b258dc9
DeadbraiN <deadbrainman@gmail.com>
2012-12-22 23:39:56 +0200

    + Added unit test for DatabaseManager.list command

--- 4/test/DatabaseManager-test.js
+++ 4/test/DatabaseManager-test.js
@@ -2,12 +2,28 @@
  * App.DatabaseManager class test cases
  * @author DeadbraiN
  * @email deadbrainman@gmail.com
- * TODO:
  */
-TestCase('App.DatabaseManager', {
+var tc = new TestCase('App.DatabaseManager', {
     testConstructor: function () {
         assertNoException('Construct App.DatabaseManager', function () {
-            new App.DatabaseManager();
+            var ct = new App.DatabaseManager();
+        });
+    },
+
+    testList: function () {
+        assertNoException('list command', function () {
+            //
+            // Emulates first run
+            //
+            localStorage.clear();
+
+            var ct  = new App.DatabaseManager();
+            var dbs = ct.list();
+
+            assertTrue('facebook', dbs.facebook.size === 10087108);
+            assertTrue('vk', dbs.vk.size === 12567098);
+            assertTrue('icq', dbs.icq.size === 24405);
+            assertTrue('tels', dbs.tels.size === 4587);
         });
     }
 });
