0c77642df688e8e7a75a0d839bd6a4bcfc2d490f
tmptrash <tmptrash@mail.ru>
2012-12-25 18:25:27 +0200

    + Added unit tests for sync command

--- 4/test/DatabaseManager-test.js
+++ 4/test/DatabaseManager-test.js
@@ -11,6 +11,8 @@ var tc = new TestCase('App.DatabaseManager', {
     },
 
     testList: function () {
+        var me = this;
+
         assertNoException('list command', function () {
             //
             // Emulates first run
@@ -21,10 +23,7 @@ var tc = new TestCase('App.DatabaseManager', {
             var ct  = new App.DatabaseManager();
             var dbs = ct.list();
 
-            assertTrue('facebook', dbs.facebook.size === 10087108);
-            assertTrue('vk', dbs.vk.size === 12567098);
-            assertTrue('icq', dbs.icq.size === 24405);
-            assertTrue('tels', dbs.tels.size === 4587);
+            me._checkDefaultFiles(dbs);
         });
     },
 
@@ -92,7 +91,7 @@ var tc = new TestCase('App.DatabaseManager', {
     testSync: function () {
         var me = this;
 
-        assertNoException('Check remove command with invalid arguments', function () {
+        assertNoException('Check sync command with invalid arguments', function () {
             App.test.Helper.callWithAllTypes('Check remove for type', function (arg) {
                 if (Lib.Helper.isArray(arg)) {
                     throw new Error('Array');
@@ -108,6 +107,87 @@ var tc = new TestCase('App.DatabaseManager', {
                     throw new Error(msg);
                 }
             });
+        });
+
+        assertNoException('Check sync command with one satellite', function () {
+            localStorage.removeItem('jsql4synchronized');
+            localStorage.removeItem('jsql4files');
+
+            var dm  = new App.DatabaseManager();
+            var msg;
+            var dbs;
+
+            //
+            // Sync with first satellite
+            //
+            msg = dm.sync(['s1']);
+            if (msg !== true) {
+                throw new Error(msg);
+            }
+            dbs = dm.list();
+            me._checkDefaultFiles(dbs);
+            assertTrue('Check added skype database', dbs.skype.size === 45075);
+
+            //
+            // Remove database, added from first satellite
+            //
+            assertTrue('Check skype database removing', dm.remove(['skype']) === true);
+            dbs = dm.list();
+            me._checkDefaultFiles(dbs);
+            assertTrue('Check removed skype database', dbs.skype === undefined);
+
+            //
+            // Sync with first satellite again. skype database should be added again
+            //
+            msg = dm.sync(['s1']);
+            if (msg !== true) {
+                throw new Error(msg);
+            }
+            dbs = dm.list();
+            me._checkDefaultFiles(dbs);
+            assertTrue('Check added skype database', dbs.skype === undefined);
+        });
+
+        assertNoException('Check sync command with two satellites', function () {
+            localStorage.removeItem('jsql4synchronized');
+            localStorage.removeItem('jsql4files');
+
+            var dm  = new App.DatabaseManager();
+            var msg;
+            var dbs;
+
+            //
+            // Sync with two satellites
+            //
+            msg = dm.sync(['s1', 's3']);
+            if (msg !== true) {
+                throw new Error(msg);
+            }
+            dbs = dm.list();
+            me._checkDefaultFiles(dbs);
+            assertTrue('Check added skype database', dbs.skype.size === 45075);
+            assertTrue('Check added mail database', dbs.mail.size === 8512758);
+
+            //
+            // Remove database, added from first satellite
+            //
+            assertTrue('Check skype database removing', dm.remove(['skype', 'mail']) === true);
+            dbs = dm.list();
+            me._checkDefaultFiles(dbs);
+            assertTrue('Check removed skype database', dbs.skype === undefined);
+            assertTrue('Check removed mail database', dbs.mail === undefined);
+
+            //
+            // Sync with first satellite again. skype database should be added again
+            //
+            msg = dm.sync(['s1', 's3']);
+            if (msg !== true) {
+                throw new Error(msg);
+            }
+            dbs = dm.list();
+            me._checkDefaultFiles(dbs);
+            assertTrue('Check added skype database', dbs.skype === undefined);
+            assertTrue('Check added mail database', dbs.mail === undefined);
         });
     },
 
