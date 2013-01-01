2b8f9dac87834dfd80253b1767da13e19c0d02dc
DeadbraiN <deadbrainman@gmail.com>
2013-01-01 23:53:31 +0200

    * Fixed: log event didn't work

--- 4/js/Scenario.js
+++ 4/js/Scenario.js
@@ -172,7 +172,7 @@ App.Scenario = speculoos.Class({
      * @private
      */
     _onDatabaseManagerLog: function (msg) {
-        this._terminal.WriteLine(msg);
+        this._terminal.console.WriteLine(msg);
     },
 
     /**
